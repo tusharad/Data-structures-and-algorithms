83d4d7d2606ca399048a7091f92e39b9cfc40de3
Tushar <tusharadhatrao@gmail.com>
2021-01-09 15:44:01 +0530

    added insert and delete node.

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -15,13 +15,14 @@ void printList(node *curr)
         printf("%d-> ", curr->data);
         curr = curr->next;
     }
+    printf("\n");
 }
 
-node *createNode(int data)
+node *createNode(int val)
 {
     node *temp = NULL;
     temp = (node *)malloc(sizeof(node));
-    temp->data = data;
+    temp->data = val;
     temp->next = NULL;
     return temp;
 }
@@ -44,8 +45,47 @@ node *createList()
     return head;
 }
 
+node *traverse(node *curr, int pos)
+{
+    int i;
+    for (i = 0; i < pos; i++)
+        curr = curr->next;
+    return curr;
+}
+
+void insertNode(node *head, int pos, int val)
+{
+    if (pos > head->data || pos < 1)
+    {
+        return;
+    }
+    node *curr = head, *temp;
+    if (pos == head->data) //last position
+        curr = traverse(curr, pos);
+    else
+        curr = traverse(curr, pos - 1);
+
+    temp = createNode(val);
+    temp->next = curr->next;
+    curr->next = temp;
+    head->data++;
+}
+
+void deleteNode(node *head, int pos)
+{
+    node *curr = head, *temp;
+    curr = traverse(curr, pos - 1);
+    temp = curr->next;
+    curr->next = curr->next->next;
+    free(temp);
+}
+
 void main()
 {
     node *head = createList();
+    printList(head);
+    insertNode(head, 3, 100);
+    printList(head);
+    deleteNode(head, 4);
     printList(head);
 }
